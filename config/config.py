c2f05d6cfdb80daf65db8317ba2ddffa69e35672
MukundaShanbogh <shanboghmukund11@gmail.com>
2026-08-05 14:12:00 +0530

    corrected typo

--- config/config.py
+++ config/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    Base_url = os.getenv("Base_url")
-    user_name = os.getenv("User_Name")
-    password = os.getenv("Password")
+    Base_url = os.getenv("BASE_URL")
+    user_name = os.getenv("USER_NAME")
+    password = os.getenv("PASSWORD")
